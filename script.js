42951497b4f44bbf4c21cb22dab5a448a7edaa7c
juno lee <lejuno80@gmail.com>
2017-07-24 06:49:02 -0500

    adding blog file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-const menuButton = () => {
+const menuButton = document.querySelectorAll('#navbar'.li);
+
+menuButton.addEventListener = ('click', () => {
+	menuButton.style.display = 'padding: 40px;'
+
 	
-}
+});
