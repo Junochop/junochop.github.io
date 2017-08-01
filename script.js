b7dfc2817e40bc4244c1708468aae111ca9762aa
juno lee <lejuno80@gmail.com>
2017-08-01 18:06:28 -0500

    updated nav bar

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-const menuButton = document.querySelectorAll('#navbar'.li);
+//const menuButton = document.querySelectorAll('#navbar'.li);
 
-menuButton.addEventListener = ('click', () => {
-	menuButton.style.display = 'padding: 40px;'
+//menuButton.addEventListener = ('click', () => {
+	//menuButton.style.display = 'padding: 40px;'
 
 	
-});
+//});
+
+var blog1 {
+	title: "",
+	date: "",
+	pic: "",
+	description: " "
+};
+
+var blog2 {
+	title: "",
+	date: "",
+	pic: "",
+	description: " "
+};
+
+var blog3 {
+	title: "",
+	date: "",
+	pic: "",
+	description: " "
+};
+
+var blog4 {
+	title: "",
+	date: "",
+	pic: "",
+	description: " "
+};
+
+var blog5 {
+	title: "",
+	date: "",
+	pic: "",
+	description: " "
+};
